35d6d74a56cc3bee730c3b4c05c9e96e875b32e3
JinyoungHeo <yuchae1118@gmail.com>
2015-10-29 14:48:52 +0900

    Create lab07_RDB.php

---
+++ lab07_RDB.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DB query</title>
+</head>
+<body>
+	<form action="RDB.php" method="post">
+		<div>
+		<input type="text" name="db_name"/>
+		<textarea rows="4" cols="100" name="query"></textarea>
+		<input type="submit" name="submit"/>
+		</div>
+	</form>
+
+		<?php
+			$db_name = $_POST["db_name"];
+			$db_query = $_POST["query"];
+				$db = new PDO("mysql:dbname=$db_name","root","root");
+				$rows = $db->query($db_query); ?>
+				<ul>
+				<?php
+				if (!empty($rows)) {
+					foreach ($rows as $r) { ?>
+						<li><?= implode(" ", $r) ?></li>
+				<?php	
+					}
+				} ?>
+
+</body>
+</html>
